fb72d12a04b9bdabfdf8811141b7be6bf352dfcf
totoroJyunn <45234186+totoroJyunn@users.noreply.github.com>
2022-09-01 11:35:49 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,24 +2,24 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: 'totorowl/Totoro-s-World-Literature-2fc99e2976df40dea94699150eb52611',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Totoro's World Literature',
+  domain: 'https://www.notion.so/totorowl/Totoro-s-World-Literature-2fc99e2976df40dea94699150eb52611',
+  author: 'Totoro',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  twitter: 'none1',
+  github: 'none2',
+  linkedin: 'none3',
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
 
